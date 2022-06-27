8a84174f665932c5ee962c3586b7acf38294985c
latheefp <latheefp@gmail.com>
2022-06-27 21:38:55 +0300

    Update command.sh

--- ingress/command.sh
+++ ingress/command.sh
@@ -1,1 +1,17 @@
 #https://docs.nginx.com/nginx-ingress-controller/installation/installation-with-manifests/
+$ git clone https://github.com/nginxinc/kubernetes-ingress.git --branch v2.2.2
+$ cd kubernetes-ingress/deployments
+kubectl apply -f common/ns-and-sa.yaml
+kubectl apply -f rbac/rbac.yaml
+kubectl apply -f rbac/ap-rbac.yaml
+kubectl apply -f rbac/apdos-rbac.yaml
+kubectl apply -f common/default-server-secret.yaml
+kubectl apply -f common/nginx-config.yaml
+kubectl apply -f common/ingress-class.yaml
+kubectl apply -f common/crds/k8s.nginx.org_virtualservers.yaml
+kubectl apply -f common/crds/k8s.nginx.org_virtualserverroutes.yaml
+kubectl apply -f common/crds/k8s.nginx.org_transportservers.yaml
+kubectl apply -f common/crds/k8s.nginx.org_policies.yaml
+kubectl apply -f common/crds/k8s.nginx.org_globalconfigurations.yaml
+
+
